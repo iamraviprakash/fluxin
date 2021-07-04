72514d2eb5fae2c93562178aa80bf77324d1bb2b
Ravi Prakash <raviprakash.coder@gmail.com>
2021-07-04 12:41:40 +0530

    feat: bounding views and stores

--- dispatcher.js
+++ dispatcher.js
@@ -15,15 +15,36 @@
 var Dispatcher = function () {
   return {
     _stores: [],
-    // throwing an error if no expected update method exists
     register: function (store) {
-      this._stores.push({ store: store });
+      // throw error if no update method exists
+      if (!store || !store.update) {
+        throw new Error(
+          "You should provide a store that has an `update` method."
+        );
+      } else {
+        var consumers = [];
+
+        var change = function () {
+          consumers.forEach(function (consumer) {
+            consumer(store);
+          });
+        };
+
+        // collect the consumer functions
+        var subscribe = function (consumer, noInit) {
+          consumers.push(consumer);
+          !noInit ? consumer(store) : null;
+        };
+
+        this._stores.push({ store, change });
+        return subscribe;
+      }
     },
     // check all stores for update
     dispatch: function (action) {
       if (this._stores.length > 0) {
         this._stores.forEach(function (entry) {
-          entry.store.update(action);
+          entry.store.update(action, entry.change);
         });
       }
     },
